02cfb600dc22ca5dd0039f3aba649e34723c19ab
Jeremy Park <jeremyjpark@gmail.com>
2015-10-28 22:38:30 +0000

    geolocation woes

--- clicktime_coffee_donut.js
+++ clicktime_coffee_donut.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+var pos;
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
@@ -9,11 +10,11 @@ function initMap() {
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
+      pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      alert(pos);
+      alert(pos.lat);
       infoWindow.setPosition(pos);
       infoWindow.setContent('Location found.');
       map.setCenter("hi");
