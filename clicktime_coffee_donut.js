159463a98c90d29d80e67239fa865cfce3997035
Jeremy Park <jeremyjpark@gmail.com>
2015-10-28 23:51:22 +0000

    have current location and clicktime geocoded

--- clicktime_coffee_donut.js
+++ clicktime_coffee_donut.js
@@ -1,41 +1,51 @@
 $(document).ready(function() {
-var origin;
-var destination 
-function initMap() {
-  // Geocode the destination
-  var geocoder = new google.maps.Geocoder();
-  geocodeAddress(geocoder);
-  // Find the Current Location w/ html5 geolocation
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      origin = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-    }, function() {
-      ;
-    });
-  }
-  // Find places near Clicktime
+  var origin;
+  var destination;
 
-  // Calculate route w/ waypoints and options
+  function init() {
+    // Geocode the destination
+    var geocoder = new google.maps.Geocoder();
+    geocodeAddress(geocoder);
+    // Find the Current Location w/ html5 geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        origin = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      }, function() {
+        ;
+      });
+    }
+    // Find places near Clicktime
+
+    // Calculate route w/ waypoints and options
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
+    directionsDisplay.setPanel(document.getElementById('directions'));
   }
 
   function geocodeAddress(geocoder) {
     var clicktime_address = "282 2nd Street 4th floor, San Francisco, CA 94105";
     geocoder.geocode({'address': clicktime_address}, function(results, status) {
       if (status === google.maps.GeocoderStatus.OK) {
-        destination = {
-          lat: results[0].geometry.location.lat(),
-          lng: results[0].geometry.location.lng()
-        };
+        destination = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
     });
   }
 
-  initMap();
-  console.log(origin)
-  console.log(destination)
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: origin,
+      destination: destination,
+      travelMode: google.maps.TravelMode.DRIVING
+    }, function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+  init();
 });
