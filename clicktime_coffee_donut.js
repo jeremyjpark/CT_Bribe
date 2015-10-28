4ae779d263bf8054140eee47048d0774f6895453
Jeremy Park <jeremyjpark@gmail.com>
2015-10-28 21:36:41 +0000

    testing initialLocation

--- clicktime_coffee_donut.js
+++ clicktime_coffee_donut.js
@@ -1,10 +1,37 @@
 $(document).ready(function() {
   var map;
+  var initialLocation;
+  var browserSupportFlag = new Boolean();
+
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
+      center: {initialLocation.lat: -34.397, initialLocation.lng: 150.644},
       zoom: 8
     });
   }
+
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+    }
+    map.setCenter(initialLocation);
+  }
+
   initMap();
 });
